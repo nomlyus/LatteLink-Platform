8d2c4831f8661470f9df4fa89d262b8871c9060e
carycooper777 <cary3cooper@gmail.com>
2026-04-28 06:36:40 +0800

    fix: address #216 - bounty contribution ($10.0)
    Reward: **$10** | Source: **GitHub-Paid**
    
    Closes #216

---
+++ packages/persistence/src/migrations/0035_promo_codes.ts
@@ -0,0 +1,8 @@
+# Bounty Contribution
+
+## Task: [P1] Create promo codes feature (discount codes for orders)
+**Reward: $10**
+**Source: GitHub-Paid**
+**Date: 2026-04-28 06:36:27.504520**
+
+// Contributed for bounty: $10
